9d2b6a92548e57c125f1185591b804a34e1afaac
unani <unani92@naver.com>
2020-09-15 15:57:42 +0900

    S03P22A505-25 음악 데이터 모델링

--- backend/music/admin.py
+++ backend/music/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Genre, Artist, Album, Song
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
--- backend/music/models.py
+++ backend/music/models.py
@@ -3,18 +3,21 @@ from django.db import models
 # Create your models here.
 class Genre(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
+    num = models.CharField(max_length=200, null=True)
 
 class Artist(models.Model):
-    artist_id = models.IntegerField(primary_key=True)
+    # artist_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
-    debut = models.CharField(max_length=200)
+    debue = models.CharField(max_length=200)
     img = models.URLField(null=True)
+    type = models.CharField(max_length=200)
+    member = models.CharField(max_length=200, null=True)
 
 class Album(models.Model):
-    album_id = models.IntegerField(primary_key=True)
+    # album_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     like = models.IntegerField()
-    genre = models.ManyToManyField(
+    genres = models.ManyToManyField(
         Genre,
         related_name= "album_genres"
     )
@@ -23,11 +26,11 @@ class Album(models.Model):
         related_name="artist_albums",
         on_delete=models.CASCADE
     )
-    intro = models.TextField(null=True)
     img = models.URLField(null=True)
+    released_date = models.CharField(max_length=200)
 
 class Song(models.Model):
-    song_id = models.IntegerField(primary_key=True)
+    # song_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     like = models.IntegerField()
     genre = models.ManyToManyField(
@@ -39,11 +42,11 @@ class Song(models.Model):
         related_name='album_songs',
         on_delete=models.CASCADE
     )
-    artist = models.ForeignKey(
+    artist = models.ManyToManyField(
         Artist,
         related_name='artist_songs',
-        on_delete=models.CASCADE
     )
     img = models.URLField(null=True)
     lyric = models.TextField(null=True)
     released = models.CharField(max_length=200)
+    type = models.CharField(max_length=200, null=True)
