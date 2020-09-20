984c1b4c9f526a882569c958b4680c80129097a5
unani <unani92@naver.com>
2020-09-20 22:11:06 +0900

    어드민 사이트에서 디비 모델링 확인 가능

--- backend/music/admin.py
+++ backend/music/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Genre, Artist, Album, Song
+from .models import *
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Song)
+admin.site.register(Log)
+admin.site.register(AlbumComment)
+admin.site.register(SongComment)
