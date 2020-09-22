588ca8e56844f1d629016498fd9f3c87c11e53ad
unani <unani92@naver.com>
2020-09-22 14:49:46 +0900

    S03P23A505-19 [BE] 음악, 앨범, 아티스트 디테일 정보 컨트롤러 구현

--- backend/music/urls.py
+++ backend/music/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:emotion_type>/', views.SongType.as_view())
+    path('<str:emotion_type>/', views.SongType.as_view()),
+    path('<str:category>/<int:pk>', views.SongDetail.as_view())
 ]
--- backend/music/views.py
+++ backend/music/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes, APIView
 from rest_framework.permissions import IsAuthenticated
-from .models import Song
-from .serializers import SongSerializer
+from .models import Song, Album, Artist
+from .serializers import SongSerializer, AlbumSerializer, ArtistSerializer
 # Create your views here.
 
 class SongType(APIView):
     def get(self, request, emotion_type):
         songs = Song.objects.filter(type__exact=emotion_type).order_by('-like')[:10]
         serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+
+class SongDetail(APIView):
+    def get(self, request,category, pk):
+        if category == 'song':
+            song = get_object_or_404(Song, pk=pk)
+            serializer = SongSerializer(song)
+        elif category == 'album':
+            album = get_object_or_404(Album, pk=pk)
+            serializer = AlbumSerializer(album)
+        else:
+            artist = get_object_or_404(Artist, pk=pk)
+            serializer = ArtistSerializer(artist)
         return Response(serializer.data)
