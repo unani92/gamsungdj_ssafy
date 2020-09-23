838ba32434b54c22491a22d3dcecb56c08913cec
오지수 <wltn0213@gmail.com>
2020-09-23 20:09:50 +0900

    회원가입 프로필 입력 완료

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,13 +1,30 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from .models import UserProfile
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    avatar = serializers.ImageField(source='userprofile.avatar')
+    gender = serializers.CharField(source='userprofile.gender')
+    age = serializers.CharField(source='userprofile.age')
+    is_signed_up = serializers.BooleanField(source="userprofile.is_signed_up")
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'avatar', 'username', 'age', 'gender')
+        fields = ('id', 'email', 'username', 'password', 'avatar', 'gender', 'age', 'is_signed_up')
+
+    def create(self, validated_data, instance=None):
+        avatar = validated_data.pop('avatar')
+        gender = validated_data.pop('gender')
+        age = validated_data.pop('age')
+        profile_data = { 'avatar': avatar, 'gender': gender, 'age': age, 'is_signed_up': True }
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        UserProfile.objects.update_or_create(user=user, **profile_data)
+        return user
 
 
--- backend/music/models.py
+++ backend/music/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from accounts.models import User
 from django.conf import settings
 
 # Create your models here.
