6e9f680708a8191d3359cc28516ad5950ace078a
오지수 <wltn0213@gmail.com>
2020-10-02 18:32:05 +0900

    S03P23A505-80 최근 재생목록 구현 중

---
+++ frontend/src/components/User/MyMusicLog.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    페이지 준비중입니다.
+</div>
+  
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+import http from '@/utils/http-common'
+export default {
+    data() {
+        return {
+            logs: []
+
+        }
+    },
+
+    computed: {
+        ...mapState(['user']),
+        ...mapGetters(['config'])
+    },
+    created() {
+        http.get('log/',{}, this.config)
+        .then((res) => {
+            this.logs = res.data
+        })
+    }
+
+}
+</script>
+
+<style>
+
+</style>
