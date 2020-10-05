2595f0f22b45a5a45833f4345cffa05a13db8d50
김유창 <oyes9316@naver.com>
2020-10-05 10:08:30 +0900

    랜딩페이지 제거

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    component: () => import("./views/home"),
+    component: () => import("./views/app"),
     redirect: `${adminRoot}/main`,
   },
   {
