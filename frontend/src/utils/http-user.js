6747472b825cfc518321e6142c7e7ad7f642dd02
unani <unani92@naver.com>
2020-12-17 14:58:59 +0900

    fix baseurl

--- frontend/src/utils/http-user.js
+++ frontend/src/utils/http-user.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'http://j3a505.p.ssafy.io:8000/api/accounts/',
-    baseURL: 'http://ec2-3-35-37-204.ap-northeast-2.compute.amazonaws.com/api/accounts/',
+    baseURL: 'http://ec2-3-35-37-204.ap-northeast-2.compute.amazonaws.com:8000/api/accounts/',
 })
